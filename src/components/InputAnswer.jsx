f52612ff2d4d5a02b4c725cef6710d7c3ed171b5
modusami <128193218+modusami@users.noreply.github.com>
2024-03-13 14:35:39 -0400

    Add ShowInput Box Functionaloty

--- src/components/InputAnswer.jsx
+++ src/components/InputAnswer.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "../styles/answer.css";
-const InputAnswer = ({ answer, handleAnswer }) => {
-	return (
+const InputAnswer = ({ answer, handleAnswer, handleSubmit, showBox }) => {
+	return showBox ? (
 		<div id="answer-div">
 			<input type="text" name="answer" id="answer" value={answer} onChange={handleAnswer} />
-			<button id="answer-btn">Enter</button>
+			<button onClick={handleSubmit} type="submit" id="answer-btn">
+				Submit
+			</button>
 		</div>
+	) : (
+		<div id="answer-div"></div>
 	);
 };
 
