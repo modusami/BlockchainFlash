31e0fc7de568065f29fd07bcdfd11207bd1240a9
modusami <128193218+modusami@users.noreply.github.com>
2024-03-13 13:36:37 -0400

    Remove import | Reaarange Arrows

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,5 @@
 import "../styles/flashcard.css";
 import blockchain_data from "../data/blockchain.js";
-import bitcoin from "/bitcoin.png";
 
 const Card = (props) => {
 	const answers = blockchain_data.map((value) => value.answer);
@@ -9,14 +8,14 @@ const Card = (props) => {
 	return (
 		<>
 			<div id="card" onClick={props.toggleCard}>
+				<div onClick={props.switchCards} id="left-card-arrow" className="card-arrow">
+					<i className="fa-solid fa-arrow-left"></i>
+				</div>
+				<div onClick={props.switchCards} id="right-card-arrow" className="card-arrow">
+					<i className="fa-solid fa-arrow-right"></i>
+				</div>
 				<div id="card-inner" className={props.showAnswer ? "flipped" : ""}>
 					<div className="front">
-						<div id="left-card-arrow" className="card-arrow">
-							<i class="fa-solid fa-arrow-left"></i>
-						</div>
-						<div id="right-card-arrow" className="card-arrow">
-							<i class="fa-solid fa-arrow-right"></i>
-						</div>
 						{/* <img id="card-img" src={bitcoin} alt="bitcoin-image" /> */}
 						{props.showDefault
 							? "Click here to show the back of the card"
@@ -24,7 +23,7 @@ const Card = (props) => {
 					</div>
 					<div className="back">
 						{props.showDefault
-							? "Click the Next Card button below to begin"
+							? "Click the Next Arrow below to begin"
 							: answers[props.index]}
 					</div>
 				</div>
